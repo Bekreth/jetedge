59e2204e36cbaa5413704f45b0ed4a07601f9dc6
Jason You <Silven1117@hotmail.com>
2018-03-08 11:47:09 -0500

    Upgraded LongLimiter to take origin or bound or both for random generation (#90)
    * Revert "Allows for user to pass in objects to be used as constructor values. (#80)"
    
    This reverts commit 80b6203ecdd41b1d7b6acf001211db624520c7a3.
    
    * Revert "Revert "Allows for user to pass in objects to be used as constructor values. (#80)""
    
    This reverts commit 1845da6782994d042eca8e2aef28dc85e1c3706e.
    
    * Upgraded LongLimiter to take origin or bound or both for random generation
    
    * addressed comments and updated test cases
    
    * removed the <p>
    
    * updated constructor to handle exception/ added an unit test

---
+++ src/test/java/com/rainbowpunch/jetedge/core/limiters/common/java/LongLimiterTest.java
@@ -0,0 +1,66 @@
+package com.rainbowpunch.jetedge.core.limiters.common.java;
+
+import com.rainbowpunch.jetedge.core.exception.LimiterConstructionException;
+import com.rainbowpunch.jetedge.core.limiters.primitive.LongLimiter;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LongLimiterTest {
+
+    private Random random = new Random();
+
+    @Test
+    public void testDefaultLongLimiter() {
+        LongLimiter longSupplier = new LongLimiter();
+        Long minComparison = 0L;
+        Long maxComparison = Long.MAX_VALUE;
+
+        for (int i = 0; i < 100; i++) {
+            Long generatedValue = longSupplier.generateSupplier(random).get();
+            assertEquals(-1, minComparison.compareTo(generatedValue));
+            assertEquals(1, maxComparison.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testBoundedLongLimiter() {
+        Long bound = 10000000000L;
+        LongLimiter longSupplier = new LongLimiter(bound);
+        Long comparison = bound;
+        for (int i = 0; i < 100; i++) {
+            Long generatedValue = longSupplier.generateSupplier(random).get();
+            assertEquals(1, comparison.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testRangedLongLimiter() {
+        Long inclusiveMin = 100000L;
+        Long exclusiveMax = 10000000000L;
+        LongLimiter longSupplier = new LongLimiter(inclusiveMin, exclusiveMax);
+
+        for (int i = 0; i < 100; i++) {
+            Long generatedValue = longSupplier.generateSupplier(random).get();
+            assertTrue(inclusiveMin.compareTo(generatedValue) == -1 || inclusiveMin.compareTo(generatedValue) == 0);
+            assertEquals(1, exclusiveMax.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testLongLimiterSeedablility() {
+        Random seededRandom = new Random(100);
+        LongLimiter longSupplier = new LongLimiter();
+        Long comparison = 6659363606675156442L;
+        Long generatedValue = longSupplier.generateSupplier(seededRandom).get();
+        assertEquals(0, comparison.compareTo(generatedValue));
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testBoundSmallerThanOrigin(){
+        new LongLimiter(Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+}
