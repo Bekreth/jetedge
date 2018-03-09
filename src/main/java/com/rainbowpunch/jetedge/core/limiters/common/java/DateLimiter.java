eacb4ec25155fd54f6c5b148566ded329e1b306c
Jason You <Silven1117@hotmail.com>
2018-03-09 14:28:49 -0500

    Upgraded DateLimiter (#93)
    * Revert "Allows for user to pass in objects to be used as constructor values. (#80)"
    
    This reverts commit 80b6203ecdd41b1d7b6acf001211db624520c7a3.
    
    * Revert "Revert "Allows for user to pass in objects to be used as constructor values. (#80)""
    
    This reverts commit 1845da6782994d042eca8e2aef28dc85e1c3706e.
    
    * Upgraded LongLimiter to take origin or bound or both for random generation
    
    * addressed comments and updated test cases
    
    * removed the <p>
    
    * updated constructor to handle exception/ added an unit test
    
    * added ranged long/date feature for DateLimiter

--- src/main/java/com/rainbowpunch/jetedge/core/limiters/common/java/DateLimiter.java
+++ src/main/java/com/rainbowpunch/jetedge/core/limiters/common/java/DateLimiter.java
@@ -1,5 +1,6 @@
 package com.rainbowpunch.jetedge.core.limiters.common.java;
 
+import com.rainbowpunch.jetedge.core.exception.LimiterConstructionException;
 import com.rainbowpunch.jetedge.core.limiters.Limiter;
 
 import java.util.Date;
@@ -8,13 +9,70 @@ import java.util.function.Supplier;
 
 /**
  * A limiter for Date data fields.
+ * DateLimiter will take in either one or two epochmilisecond in long
+ * Default origin is 0L (January 1, 1970 12:00:00 AM)
+ * Default bound is 4102444800000L (January 1, 2100 12:00:00 AM)
  */
 public class DateLimiter implements Limiter<Date> {
 
+    private final static Long DEFAULT_ORIGIN = 0L;
+    private final static Long DEFAULT_BOUND = 4102444800000L;
+    private final Long origin;
+    private final Long bound;
+
+    /**
+     * This constructor will generate random date between January 1, 1970 12:00:00 AM(inclusive) and January 1, 2100 12:00:00 AM(exclusive)
+     */
+    public DateLimiter() {
+        this(DEFAULT_ORIGIN, DEFAULT_BOUND);
+    }
+
+    /**
+     * This constructor will generate random date between January 1, 1970 12:00:00 AM(inclusive) and boundDate(exclusive)
+     *
+     * @param boundDate exclusive right bound
+     */
+    public DateLimiter(Date boundDate) {
+        this(DEFAULT_ORIGIN, boundDate.getTime());
+    }
+
+    /**
+     * This constructor will generate random date between originDate(inclusive) and boundDate(exclusive)
+     *
+     * @param originDate inclusive left bound
+     * @param boundDate exclusive right bound
+     */
+    public DateLimiter(Date originDate, Date boundDate) {
+        this(originDate.getTime(), boundDate.getTime());
+    }
+
+    /**
+     * This constructor will create new Date with boundLong as epochmilisecond and generate random date between January 1, 1970 12:00:00 AM
+     * (inclusive) and new Date(boundLong)(exclusive)
+     *
+     * @param boundLong exclusive right bound
+     */
+    public DateLimiter(Long boundLong) {
+        this(DEFAULT_ORIGIN, boundLong);
+    }
+
+    /**
+     * This constructor will create new Dates with originLong/boundLong as epochmilisecond and generate random date between new Date(originLong)
+     * (inclusive) and new Date(boundLong)(exclusive)
+     *
+     * @param originLong inclusive left bound
+     * @param boundLong exclusive right bound
+     */
+    public DateLimiter(Long originLong, Long boundLong) {
+        if (boundLong < originLong || originLong < 0 || boundLong < 0) {
+            throw new LimiterConstructionException("Origin can't be greater than the bound!");
+        }
+        this.origin = originLong;
+        this.bound = boundLong;
+    }
+
     @Override
     public Supplier<Date> generateSupplier(Random random) {
-        return () -> {
-            return new Date();
-        };
+        return () -> new Date(random.longs(origin, bound).findFirst().getAsLong());
     }
 }
---
+++ src/test/java/com/rainbowpunch/jetedge/core/limiters/common/java/DateLimiterTest.java
@@ -0,0 +1,114 @@
+package com.rainbowpunch.jetedge.core.limiters.common.java;
+
+import com.rainbowpunch.jetedge.core.exception.LimiterConstructionException;
+import com.rainbowpunch.jetedge.core.limiters.primitive.LongLimiter;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DateLimiterTest {
+
+    private Random random = new Random();
+
+    @Test
+    public void testDefaultDateLimiter() {
+        DateLimiter dateSupplier = new DateLimiter();
+        Date minComparison = new Date(0L);
+        Date maxComparison = new Date(Long.MAX_VALUE);
+
+        for (int i = 0; i < 100; i++) {
+            Date generatedValue = dateSupplier.generateSupplier(random).get();
+            assertEquals(-1, minComparison.compareTo(generatedValue));
+            assertEquals(1, maxComparison.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testBoundedDateLimiterWithDate() {
+        Date bound = new Date(10000000000L);
+        DateLimiter dateSupplier = new DateLimiter(bound);
+
+        for (int i = 0; i < 100; i++) {
+            Date generatedValue = dateSupplier.generateSupplier(random).get();
+            assertEquals(1, bound.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testRangedDateLimiterWithDate() {
+        Date inclusiveMin = new Date(100000L);
+        Date exclusiveMax = new Date(10000000000L);
+        DateLimiter dateSupplier = new DateLimiter(inclusiveMin, exclusiveMax);
+
+        for (int i = 0; i < 100; i++) {
+            Date generatedValue = dateSupplier.generateSupplier(random).get();
+            assertTrue(inclusiveMin.compareTo(generatedValue) == -1 || inclusiveMin.compareTo(generatedValue) == 0);
+            assertEquals(1, exclusiveMax.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testBoundedDateLimiterWithLong() {
+        Long boundLong = 10000000000L;
+        Date bound = new Date(boundLong);
+        DateLimiter dateSupplier = new DateLimiter(boundLong);
+
+        for (int i = 0; i < 100; i++) {
+            Date generatedValue = dateSupplier.generateSupplier(random).get();
+            assertEquals(1, bound.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testRangedDateLimiterWithLong() {
+        Long originLong = 100000L;
+        Long boundLong = 10000000000L;
+        Date originDate = new Date(originLong);
+        Date exclusiveDate = new Date(boundLong);
+        DateLimiter dateSupplier = new DateLimiter(originLong, boundLong);
+
+        for (int i = 0; i < 100; i++) {
+            Date generatedValue = dateSupplier.generateSupplier(random).get();
+            assertTrue(originDate.compareTo(generatedValue) == -1 || originDate.compareTo(generatedValue) == 0);
+            assertEquals(1, exclusiveDate.compareTo(generatedValue));
+        }
+    }
+
+    @Test
+    public void testDateLimiterSeedablility() {
+        Random seededRandom = new Random(100);
+        DateLimiter dateSupplier = new DateLimiter();
+        Date comparison = new Date(343513556442L);
+        Date generatedValue = dateSupplier.generateSupplier(seededRandom).get();
+        assertEquals(0, comparison.compareTo(generatedValue));
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testBoundSmallerThanOrigin() {
+        new DateLimiter(Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testNegativeOriginPositiveBound() {
+        new DateLimiter(-1L, Long.MAX_VALUE);
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testPositiveOriginNegativeBound() {
+        new DateLimiter(Long.MIN_VALUE, -1L);
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testNegativeOriginNegativeBound() {
+        new DateLimiter(-1L, -1L);
+    }
+
+    @Test(expected = LimiterConstructionException.class)
+    public void testNegativeBound() {
+        new DateLimiter(Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+}
